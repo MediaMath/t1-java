72357df6f5c3150d965835d79fe55b4524a063b6
Nitesh Chauhan <nitesh.chauhan@xoriant.com>
2016-03-16 11:57:06 +0530

    T1Error class commit

---
+++ src/main/java/com/mediamath/jterminalone/models/T1Error.java
@@ -0,0 +1,16 @@
+package com.mediamath.jterminalone.models;
+
+public class T1Error {
+	
+	String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
